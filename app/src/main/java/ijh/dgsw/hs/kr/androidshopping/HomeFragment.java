282060b85b790f6c947e5d9b01d2ad25eba6c09e
gurdlwl <gurdlwl@gmail.com>
2019-10-14 20:19:06 +0900

    product image 추가 - 출처 : hun-s.com

--- app/src/main/java/ijh/dgsw/hs/kr/androidshopping/HomeFragment.java
+++ app/src/main/java/ijh/dgsw/hs/kr/androidshopping/HomeFragment.java
@@ -22,46 +22,33 @@ import ijh.dgsw.hs.kr.androidshopping.Home.ItemClickListener;
 import ijh.dgsw.hs.kr.androidshopping.Home.RecyclerAdapter;
 
 public class HomeFragment extends Fragment implements ItemClickListener {
-    private static final int INTERVAL_TIME = 3500;
+    private static final int INTERVAL_TIME = 3800;
 
+    private View rootView;
     private ViewFlipper viewFlipper;
     private RecyclerView recyclerView;
     private RecyclerAdapter adapter;
     private ArrayList<ProductBean> data;
     private ProductDBHelper dbHelper;
-    SQLiteDatabase db;
+    private SQLiteDatabase db;
 
     int images[] = {
-            R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground
+            R.drawable.slide_image_1,
+            R.drawable.slide_image_2,
+            R.drawable.slide_image_3,
+            R.drawable.slide_image_4
     };
 
     // 메인. 슬라이드 형식 화면 절반치 광고, 랜덤 상품 아래에 6개 정도 보여주기
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.activity_home_fragment, container, false);
+        rootView = inflater.inflate(R.layout.activity_home_fragment, container, false);
         viewFlipper = rootView.findViewById(R.id.imageSlide);
 
-        for(int image : images){
+        for(int image : images)
             flipperImages(image);
-        }
 
-        dbHelper = ProductDBHelper.getInstance(getContext());
-        data = dbHelper.getAllProduct();
-        db = dbHelper.getWritableDatabase();
 
-        init("product", 1, "청자켓", 73000);
-        init("product", 2,"청바지", 56000);
-        init("product", 3, "무스탕", 130000);
-        init("product", 4, "신발", 69000);
-        init("product", 5, "팔찌", 12900);
-        init("product", 6, "맨투맨", 25900);
-
-        GridLayoutManager layoutManager = new GridLayoutManager(getContext(),3);
-        recyclerView = rootView.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter = new RecyclerAdapter(data, this);
-        recyclerView.setAdapter(adapter);
+        showProduct();
 
         return rootView;
     }
@@ -76,6 +63,29 @@ public class HomeFragment extends Fragment implements ItemClickListener {
 
         viewFlipper.setInAnimation(getContext(), R.anim.slide_in_anim);
         viewFlipper.setOutAnimation(getContext(), R.anim.slide_out_anim);
+    }
+
+    private void showProduct(){
+        dbHelper = ProductDBHelper.getInstance(getContext());
+        data = dbHelper.getAllProduct();
+        db = dbHelper.getWritableDatabase();
+
+        initProduct();
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(),3);
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter(data, this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initProduct(){
+        init("product", 1, "청자켓", 73000);
+        init("product", 2,"청바지", 56000);
+        init("product", 3, "무스탕", 130000);
+        init("product", 4, "신발", 69000);
+        init("product", 5, "팔찌", 12900);
+        init("product", 6, "맨투맨", 25900);
     }
 
     @Override
